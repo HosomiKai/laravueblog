b90abfe55497a60ee6620dd8837a49dd5088f29f
hosomikai <showking111@gmail.com>
2018-10-05 20:09:24 +0800

    實戰 CRUD 使用 Laravel + Vue

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
+
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
 <div id="app">
