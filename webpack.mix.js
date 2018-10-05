b8d3846c3801b2881f8491c00faf7903f323f5e2
hosomikai <showking111@gmail.com>
2018-10-05 17:55:53 +0800

    建立一個vue component 與打包js,css範例

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,6 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
+
+mix.js('resources/js/hello.js', 'public/js')
+    .extract(['lodash','jquery','axios','vue']); //抽取全域套件
